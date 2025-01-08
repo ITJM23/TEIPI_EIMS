49c9e02dc7eaa463f3d1a214f864e1feec30fea7
John Mark Tolentino <109072387+ITJM23@users.noreply.github.com>
2025-01-08 13:36:51 +0800

    login is now sql srv

--- users/includes/db.php
+++ users/includes/db.php
@@ -1,59 +1,41 @@
 <?php
 	
-	// ============ Canteen Database ============
-		$servername1 	= "localhost";
-		$username1 		= "root";
-		$password1 		= "";
-		$dbname1 		= "canteen2";
-
-		$con = mysqli_connect($servername1, $username1, $password1, $dbname1, '3308');
-	// ============ Canteen Database END ========
-
-
-
-	// ============ HR Admin Database ============
-		$servername2 	= "localhost";
-		$username2 		= "root";
-		$password2 		= "";
-		$dbname2 		= "teipi_emp2";
-
-		$con2 = mysqli_connect($servername2, $username2, $password2, $dbname2, '3308');
-	// ============ HR Admin Database END ========	
-
-
-
-	// ============ Health Declaration Database ============
-		$servername3 	= "localhost";
-		$username3 		= "root";
-		$password3 		= "";
-		$dbname3 		= "health_declaration";
-
-		$con3 = mysqli_connect($servername3, $username3, $password3, $dbname3, '3308');
-	// ============ Health Declaration Database END ========	
-
-
-
-	// ============ Employee IMS Database ============
-		$servername4 	= "localhost";
-		$username4 		= "root";
-		$password4 		= "";
-		$dbname4 		= "eims";
-
-		$con4 = mysqli_connect($servername4, $username4, $password4, $dbname4, '3308');
-	// ============ Employee IMS Database END ========	
-
-	$serverName = "jvaserver";
-	$connectionOptions = array(
-    "Database" => "TEIPIHRDB",
-    "Uid" => "SystemDeveloper",
-    "PWD" => "admin"
+// ============ Canteen Database ============
+$serverName = "IMSERVER";
+$connectionOptions = array(
+    "Database" => "canteen2",
+    "Uid" => "sa",
+    "PWD" => "IS@Admin"
 );
 
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+$con = sqlsrv_connect($serverName, $connectionOptions);
 
-if (!$conn) {
-    die(print_r(sqlsrv_errors(), true)); // Print detailed error information
+if (!$con) {
+    die(print_r(sqlsrv_errors(), true));
+} else {
+    //echo 'Connect success to canteen2 database';
 }
+
+// ============ Canteen Database END ========
+
+
+
+// ============ Teipi_emp3 Database ============
+$serverName2 = "IMSERVER";
+$connectionOptions2 = array(
+    "Database" => "Teipi_emp3",
+    "Uid" => "sa",
+    "PWD" => "IS@Admin"
+);
+
+$con2 = sqlsrv_connect($serverName2, $connectionOptions2);
+
+if (!$con2) {
+    die(print_r(sqlsrv_errors(), true));
+} else {
+    //echo 'Connect success to Teipi_emp3 database';
+}
+
 
 
 ?>
